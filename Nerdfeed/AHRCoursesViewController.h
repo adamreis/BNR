49a7b0d1d812faf8cc5fa72d1fc7eb6dd8a5616e
Adam Reis <adamhreis@gmail.com>
2014-06-03 19:56:45 -0700

    Boilerplate for NerdFeed -- just printing the JSON response from BNR

---
+++ Nerdfeed/AHRCoursesViewController.h
@@ -0,0 +1,13 @@
+//
+//  AHRCoursesViewController.h
+//  Nerdfeed
+//
+//  Created by Adam Reis on 6/3/14.
+//  Copyright (c) 2014 Adam Reis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHRCoursesViewController : UITableViewController
+
+@end
---
+++ Nerdfeed/NerdfeedTests/NerdfeedTests.m
@@ -0,0 +1,34 @@
+//
+//  NerdfeedTests.m
+//  NerdfeedTests
+//
+//  Created by Adam Reis on 6/3/14.
+//  Copyright (c) 2014 Adam Reis. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NerdfeedTests : XCTestCase
+
+@end
+
+@implementation NerdfeedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
